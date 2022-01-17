d27040c463c93d12085c90f087a7489df651f912
cristhiandom <colarte191@unab.edu.co>
2022-01-16 21:22:45 -0500

    Agregué con funciones el taller de precios y descuentos

--- descuentos.js
+++ descuentos.js
@@ -6,12 +6,33 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
 
+    let discount;
 
+    switch(discountValue) {
+        case coupons[0]:
+            discount = 15;
+            break;
+        case coupons[1]:
+            discount = 45;
+            break;
+        case coupons[2]:
+            discount = 25;
+            break;
+    }
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento
-// });
+    const precioDescuento = calcularPrecioConDescuento(priceValue, discount);
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El resulto del precio con descuento es: " + precioDescuento;
+}
+
+const coupons = [
+    "Primera_Compra",
+    "Black_Mondá",
+    "Tengo_Down",
+];
